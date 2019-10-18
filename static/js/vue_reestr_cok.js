56020ea7ba4d962c360cec88e67585a3584be023
Anatoliy Popov <popov.anatoly@gmail.com>
2019-10-18 15:52:42 +0300

    qualification modal added

--- static/js/vue_reestr_cok.js
+++ static/js/vue_reestr_cok.js
@@ -7,6 +7,13 @@ if (document.getElementById('app_reestr_cok')) {
                 title: 'app_reestr_cok',
                 direction: null,
                 reestrCenters: [],
+                title_input: '',
+                city_input: '',
+                search_parameters: {
+                    title: '',
+                    city: '',
+                    qualifications: [],
+                },
             }
         },
         mounted() {
@@ -34,6 +41,50 @@ if (document.getElementById('app_reestr_cok')) {
                     this.$cookies.set("centers_storage_updated", "1", "1h");
                     this.reestrCenters = loaded_arr.filter(element => element.direction.includes(this.direction));
                     });
+            },
+        },
+        computed: {
+            filteredCenters: function() {
+                let search_parameters = this.search_parameters;
+                console.log('filtering...', search_parameters);
+                let centers = this.reestrCenters;
+                let search_title_string = this.title_input.trim().toLowerCase();
+
+                let show_full_reestr = true;
+                for (var key of Object.keys(this.search_parameters)) {
+                    console.log('key', key, key, search_parameters[key].length);
+                    if (search_parameters[key].length > 0) {
+                        show_full_reestr = false;
+                    }
+                }
+                if (show_full_reestr) {
+                    return centers;
+                }
+
+
+                centers = centers.filter(function(item){
+                    return (item.company.includes(search_parameters.title) ||
+                            item.short_code.includes(search_parameters.title)) &&
+                            item.city.includes(search_parameters.city) &&
+                            function(){
+                                for (var el of search_parameters.qualifications) {
+                                    if (!item.qualifications.includes(el)) {
+                                        return false
+                                    }
+                                }
+                                return true
+                            }();
+                })
+
+                return centers
+
+                // centers = centers.filter(function(item) {
+                //     if (item.short_code.toLowerCase().indexOf(search_title_string) !== -1 ||
+                //     item.company.toLowerCase().indexOf(search_title_string) !== -1) {
+                //         return item;
+                //     }
+                // })
+                // return centers
             }
         }
     })
